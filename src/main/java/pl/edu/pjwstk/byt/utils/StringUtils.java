a658e5fdd8c85e3c18c17c91cba7fcee484a9d14
mlaskowski7 <mtlaskowski7@gmail.com>
2025-11-29 23:15:10 +0100

    feat: implement category class

---
+++ src/main/java/pl/edu/pjwstk/byt/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package pl.edu.pjwstk.byt.utils;
+
+public class StringUtils {
+
+    public static boolean isNullOrBlank(String string) {
+        return string == null || string.isBlank();
+    }
+}
