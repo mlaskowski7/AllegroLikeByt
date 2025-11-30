275b38ec4cc3ee158b68b56738249e72f0ae353b
mlaskowski7 <mtlaskowski7@gmail.com>
2025-11-30 18:40:04 +0100

    feat: add max cart items func to the shopping cart

--- src/main/java/pl/edu/pjwstk/byt/ShoppingCart.java
+++ src/main/java/pl/edu/pjwstk/byt/ShoppingCart.java
@@ -7,47 +7,24 @@ import java.util.Set;
 
 public class ShoppingCart {
 
-    private static final int MAX_CART_ITEMS = 50;
+    public static final int MAX_CART_ITEMS = 50;
 
     private final Set<CartItem> cartItems = new HashSet<>();
     private LocalDateTime createdDate;
     private LocalDateTime lastUpdated;
 
-    public ShoppingCart(LocalDateTime createdDate) {
-        setCreatedDate(createdDate);
-        setLastUpdated(createdDate);
-    }
-
-    public static int getMaxCartItems() {
-        return MAX_CART_ITEMS;
+    public ShoppingCart() {
+        var now = LocalDateTime.now();
+        createdDate = now;
+        lastUpdated = now;
     }
 
     public LocalDateTime getCreatedDate() {
         return createdDate;
     }
 
-    private void setCreatedDate(LocalDateTime createdDate) {
-        if (createdDate == null) {
-            throw new IllegalArgumentException("Creation date cannot be null.");
-        }
-        if (createdDate.isAfter(LocalDateTime.now())) {
-            throw new IllegalArgumentException("Creation date cannot be in the future.");
-        }
-        this.createdDate = createdDate;
-    }
-
     public LocalDateTime getLastUpdated() {
         return lastUpdated;
-    }
-
-    public void setLastUpdated(LocalDateTime lastUpdated) {
-        if (lastUpdated == null) {
-            throw new IllegalArgumentException("Last updated date cannot be null.");
-        }
-        if (createdDate != null && lastUpdated.isBefore(createdDate)) {
-            throw new IllegalArgumentException("Last updated date cannot be before creation date.");
-        }
-        this.lastUpdated = lastUpdated;
     }
 
     public Set<CartItem> getCartItems() {
@@ -56,16 +33,27 @@ public class ShoppingCart {
 
     public void clearCart() {
         this.cartItems.clear();
-        setLastUpdated(LocalDateTime.now());
+        lastUpdated = LocalDateTime.now();
     }
 
-    public void updateCart(Product product, int quantity) {
+    public boolean updateCart(Product product, int quantity) {
         if (product == null) {
             throw new IllegalArgumentException("Product cannot be null.");
         }
 
+        var cartItemsCount = cartItems
+            .stream()
+            .mapToInt(ci -> ci.getQuantity())
+            .sum();
+
+        if (quantity + cartItemsCount > MAX_CART_ITEMS) {
+            return false;
+        }
+        
+
         var cartItem = new CartItem(1, product);
         cartItems.add(cartItem);
-        setLastUpdated(LocalDateTime.now());
+        lastUpdated = LocalDateTime.now();
+        return true;
     }
 }
